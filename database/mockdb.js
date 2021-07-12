d0ad1598a93718d4057e96907fc8f9859d97257e
Yoeri Moens <yoerimoens@gmail.com>
2021-07-12 02:19:55 +0200

    Setup Database connection and fetch users

--- database/mockdb.js
+++ database/mockdb.js
@@ -1,13 +1,16 @@
-export const users = [
-    {
-        name: 'Yoeri Moens',
-        email: 'test@gmail.com',
-        projects: [{title: 'Site upgrade'}]
-    },
-    {
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        projects: [{title: 'Site upgrade'}]
-    },
-];
+const { Client } = require('pg');
+import dotenv from "dotenv";
+dotenv.config();
 
+const client = new Client({
+    user: process.env.USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+})
+client.connect()
+client.query('SELECT * FROM USERS', (err, res) => {
+    console.log(err, res)
+    client.end()
+})
