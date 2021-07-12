ea18b081bed23e6e4e203e5edf6486986c9d2378
Yoeri Moens <yoerimoens@gmail.com>
2021-07-12 16:18:10 +0200

    Add queries to index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-const { ApolloServer, gql } = require('apollo-server');
-import { typeDefs } from './graphql/schema';
-import { resolvers } from './graphql/resolvers';
+const {ApolloServer, gql} = require('apollo-server');
+import {typeDefs} from './graphql/schema';
+import {resolvers} from './graphql/resolvers';
+import {addUser, createUsersTable, getUsers, listTables} from "./utils";
+
+// createUsersTable();
+// listTables();
+// addUser(1, 'Yoeri', 'yoeri@test.com', 'password');
+// addUser(2, 'John', 'john@test.com', 'password');
+// getUsers();
 
 const server = new ApolloServer({typeDefs, resolvers});
 
-server.listen().then(({url}:{url: string}) => {
+server.listen().then(({url}: { url: string }) => {
     console.log(`Server at ${url}`)
 });
